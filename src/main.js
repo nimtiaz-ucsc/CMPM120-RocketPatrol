561535848ae1eae8487010c4b2c7e43969bb98fa
nimtiaz <nimtiaz@ucsc.edu>
2022-04-07 18:45:37 -0700

    initial commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+let config = {
+    width: 640,
+    height: 480,
+    scene: [Menu, Play]
+};
+
+let borderUISize = config.height / 15;
+let borderPadding = borderUISize / 3;
+
+let game = new Phaser.Game(config);
