5d8317dbeffbb4cb02cd10521aece2c70f21b9a0
nimtiaz <nimtiaz@ucsc.edu>
2022-04-08 15:46:18 -0700

    added enemy ships

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,6 +6,7 @@ class Play extends Phaser.Scene {
     preload() {
         this.load.image('starfield', 'assets/starfield.png');
         this.load.image('rocket', 'assets/rocket.png');
+        this.load.image('ship', 'assets/spaceship.png');
     }
 
 
@@ -17,16 +18,20 @@ class Play extends Phaser.Scene {
 
         this.starfield = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'starfield').setOrigin(0, 0);
 
-        
+        this.p1Rocket = new Rocket(this, game.config.width/2, 431, 'rocket').setOrigin(0.5, 0);
+        this.p1Rocket.reset();
+
+        this.shipA = new Ship(this, 300, 300, 'ship');
+        this.shipB = new Ship(this, 400, 150, 'ship');
+        this.shipC = new Ship(this, 100, 200, 'ship');
+
+
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize*2, 0x00FF00).setOrigin(0, 0);
 
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-
-        this.p1Rocket = new Rocket(this, game.config.width/2, 431, 'rocket').setOrigin(0.5, 0);
-        this.p1Rocket.reset();
         
     }
 
@@ -46,6 +51,10 @@ class Play extends Phaser.Scene {
         }
 
         this.p1Rocket.update();
+
+        this.shipA.update();
+        this.shipB.update();
+        this.shipC.update();
         
     }
 }
